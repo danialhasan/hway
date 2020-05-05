0f8b8ce048e268ea6d437c929f2d0b9f88a3d0f4
Danial Hasan <danialhasan14@gmail.com>
2020-05-04 20:49:08 -0400

    adding responsive navbar

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
+function myFunction() {
+    var x = document.getElementById("navbar");
+    if (x.className === "nav") {
+        x.className += " responsive";
 
+
+    } else {
+        x.className = "nav";
+
+    }
+}
+var y = 1;
+
+function overlay() {
+    if (y = 1) {
+        document.getElementById("overlay").style.display = "block";
+        y === 0;
+    } else {
+        document.getElementById("overlay").style.display = "none";
+        y === 1;
+    }
+}

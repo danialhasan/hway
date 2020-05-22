b5e8a43e45127a0065d2a0f8893bcabda84abe70
Danial Hasan <danialhasan14@gmail.com>
2020-05-22 19:34:10 -0400

    accomodated zoomed screen sizes for resolutions of 1920p, 1600p and 1440p.

--- script.js
+++ script.js
@@ -20,3 +20,36 @@ function overlay() {
         y = 1;
     }
 }
+
+function calculateZoomWidth() {
+    var widths = [
+            2650,
+            1920,
+            1600,
+            1440
+        ];
+    var zoomLevels = [
+            1.1,
+            1.25,
+            1.5
+        ];
+
+
+    for (var i = 0; i <= widths.length - 1; i++) {
+        for (j = 0; j <= zoomLevels.length - 1; j++) {
+
+            var z = widths[i] / zoomLevels[j]; //final result
+
+            console.log(+widths[i] + 'px, zoom ' + zoomLevels[j] + '  = width of ' + z + 'px');
+
+
+        }
+        j = 0;
+        console.log(" ");
+        //        console.log(widths[i] + 'px at ' + y + ' zoom is equal to a width of ' + z);
+        //        console.log("something");
+    }
+
+
+    console.log("Working!")
+}
